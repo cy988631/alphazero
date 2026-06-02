9bced22fbfdfe99a78dfdf2b43e8a7c0d4229a5b
cjy <cjy@cjydeMacBook-Air.local>
2026-06-03 00:13:50 +0800

    mcts1

---
+++ mcts_ai.py
@@ -0,0 +1,12 @@
+from game import Board
+
+class MCTSnode():
+    def __init__(self):
+        self.
+        self.
+        self.
+        self.
+        
+
+class MCTS():
+    def mcts(self,board):
