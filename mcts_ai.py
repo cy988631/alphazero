103079747abb01ce41237c6fe7f6a60b507c7a4c
cy988631 <1181869833@qq.com>
2026-06-03 12:11:59 +0800

    mcts2

--- mcts_ai.py
+++ mcts_ai.py
@@ -1,12 +1,22 @@
 from game import Board
+import math
 
 class MCTSnode():
-    def __init__(self):
-        self.
-        self.
-        self.
-        self.
+    def __init__(self,parent,action):
+        self.parent = parent
+        self.children = {}
+        self.wins = 0
+        self.visits = 0
+        self.action = action
         
+    def get_UCB(self,c_param):
+        if self.visits == 0:
+            return float('inf')
+        else:
+            exploitation = self.wins/self.visits
+            self.c_param = c_param
+            exploration = self.c_param*math.sqrt(math.log(self.parent.visits)/self.visits)
+            return exploitation + exploration
 
 class MCTS():
-    def mcts(self,board):
+    def selection(self,board):
